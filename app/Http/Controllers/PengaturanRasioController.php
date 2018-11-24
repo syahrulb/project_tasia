1e66e60451603eebed6e73eecc004cbed18b2e40
Rozana <rozanafirdausi12@gmail.com>
2018-11-24 21:22:09 +0700

    change controller name

--- app/Http/Controllers/PengaturanRasioController.php
+++ app/Http/Controllers/PengaturanRasioController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PengaturanAkun extends Controller
+class PengaturanRasioController extends Controller
 {
     /**
      * Display a listing of the resource.
